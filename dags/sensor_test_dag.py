c13ea864463f19dfee34fcbd5c907f5d3795dd4f
You wish you were this cool <danfacespam@protonmail.com>
2024-04-28 23:53:54 +0300

    dag improvement

--- dags/sensor_test_dag.py
+++ dags/sensor_test_dag.py
@@ -8,7 +8,8 @@ from airflow.providers.microsoft.azure.hooks.wasb import WasbHook
 
 from datetime import datetime
 import zipfile
-
+# 	stock_market_source_api_key
+# "Free accounts have access to the following countries: Mexico, New Zealand, Sweden, Thailand. For more, contact us at support@tradingeconomics.com."
 some_args = {
     'owner':'airflow',
     'start_date': datetime(2024, 3, 29),
@@ -31,9 +32,9 @@ def dataset_producer_dag():
     def get_new_updates():
         import requests
         api_key = Variable.get('stock_market_source_api_key')
-        api_link= Variable.get('stock_market_source_api_key')
-        api_country = Variable.get('stock_market_country_1')
-
+        api_link= Variable.get('stock_market_source_api_link')
+        api_countries: dict[str, list[str]] = Variable.get('stock_market_countries')
+        api_country = api_countries[0[0]]
         try:
             data = requests.get(
                 url=f'https://api.tradingeconomics.com/country/{api_country}?',
@@ -53,7 +54,8 @@ def dataset_producer_dag():
     def upload_json_to_blob(json_string:str):
         import json
         json = json.loads(json_string)
-
+        api_countries: dict[str, list[str]] = Variable.get('stock_market_countries')
+        api_country = api_countries[0[0]]
         with open("../materials/data.json", "w") as outfile:
             json.dump(json, outfile, indent=4)  # Add indent for readability
         WasbHook(
@@ -62,9 +64,13 @@ def dataset_producer_dag():
         ).load_file(
             file_path="../materials/data.json",
             container_name="sparkdata",
-            blob_name=f"market_data_{Variable.get('stock_market_country_1')}_{datetime.today()}.json"
+            blob_name=f"market_data_{api_country}_{datetime.today()}.json"
         )
-        
+    extract = get_new_updates()
+    update_dataset()
+    upload_json_to_blob(extract)
+
+dataset_producer_dag()
 
         
     
