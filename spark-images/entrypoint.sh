ba757ed8dd442e4775a78a91ad7888e1f94bc804
DDSNA <79444147+DDSNA@users.noreply.github.com>
2024-10-09 23:15:29 +0300

    add spark image

--- spark-images/entrypoint.sh
+++ spark-images/entrypoint.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Set the SPARK_HOME environment variable
-export SPARK_HOME=/opt/spark
+SPARK_WORKLOAD=$1
 
-# Check if the SPARK_MODE environment variable is set to "master"
-if [ "$SPARK_MODE" == "master" ]; then
-    echo "Starting Spark Master..."
-    $SPARK_HOME/sbin/start-master.sh
-else
-    # If not in master mode, assume worker mode
-    echo "Starting Spark Worker..."
-    $SPARK_HOME/sbin/start-worker.sh $SPARK_MASTER
+echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
+
+if [ "$SPARK_WORKLOAD" == "master" ];
+then
+  start-master.sh -p 7077
+  echo "Spark Master is running"
+  sleep 360
+  ./sbin/start-connect-server.sh --packages org.apache.spark:spark-connect_2.12:3.5.1 &
+  echo "Spark Master is running with connect"
+elif [ "$SPARK_WORKLOAD" == "worker" ];
+then
+  start-worker.sh spark://192.168.0.38:7077
+  echo "Spark Worker is running"
+elif [ "$SPARK_WORKLOAD" == "history" ]
+then
+  start-history-server.sh
 fi
-
-# Keep the container running
-tail -f /dev/null
